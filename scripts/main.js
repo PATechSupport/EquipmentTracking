119164af36d1456408d56914c4f12381aead0655
wtguinness <wtguinness@aol.com>
2015-06-29 23:57:04 -0400

    Display items dynamically with additions and removals.  Return button event works with Firebase Key value.  Format for <li>'s is a work in progress.

--- scripts/main.js
+++ scripts/main.js
@@ -1,36 +1,46 @@
-'use strict'
+'use strict';
+
+
+
+
 $(document).ready(function () {
 
     // Setup Firebase reference
-    var ref = new Firebase("https://eqlog.firebaseio.com/distribute");
+    var ref = new Firebase("https://eqlog.firebaseio.com/distributed");
 
-    ref.once('value', function (snapshot) {
+    ref.on('value', function (snapshot) {
+        // On initial connection and a value change, CLEAR out DIV and APPEND updated data.
+        $('#eqOut').empty();
+        // Loop through each item in Firebase's /distribute JSON tree.
         snapshot.forEach(function (childsnapshot) {
             var key = childsnapshot.key();
             var data = childsnapshot.val();
-            $('#eqOut').append("<li class='list-group-item'>Date Out: " + data.dateOut + "Tag/Device: " + data.tagDevice + "Name: " + data.name + "Location: " + data.location + "Notes: " + data.notes + " Days Overdue: <span class='badge'>#days Overdue</span></li>");
+            $('#eqOut').append("<li class='list-group-item' data-key=" + key + ">Date Out: " + data.dateOut + "Tag/Device: " + data.tagDevice + "Name: " + data.name + "Location: " + data.location + "Notes: " + data.notes + "Key: " + key + "<span class='badge'>#days Overdue</span><button class='btn btn-primary btn-sm return' >Returned</button></li>");
 
         });
     });
 
-
+    $('#eqOut').on('click', '.return', function returnItem() {
+        console.log('Item RETURNED! ' + $(this).val());
+    });
 
     $('#save').on('click', function () {
-        console.log('Save clicked!');
-        var equipmentOut = {
-            dateOut: $('#inputDateOut').val(),
-            tagDevice: $('#inputTagDevice').val(),
-            name: $('#inputMOSName').val(),
-            // command: $('#').val(),
-            location: $('#inputLocation').val(),
-            notes: $('#inputNotes').val(),
-            dateIn: ''
-        };
-        ref.child("distributed").push(equipmentOut);
+        console.log('Form Valid: ' + $('form').valid());
+        if ($('form').valid()) {
+            var equipmentOut = {
+                dateOut: $('#inputDateOut').val(),
+                tagDevice: $('#inputTagDevice').val(),
+                name: $('#inputMOSName').val(),
+                // command: $('#').val(),
+                location: $('#inputLocation').val(),
+                notes: $('#inputNotes').val(),
+                dateIn: ''
+            };
+            ref.push(equipmentOut);
+        } else {
+            alert('Please CHECK Form!');
+        }
     });
 
-    $('#return').on('click', function () {
-        console.log('Return clicked!');
-    });
 
 });
